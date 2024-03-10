2582fb53396471bab4d30584240328ec3d7347e4
illia_pastushok <276487@student.pwr.edu.pl>
2024-03-10 12:34:59 +0100

    Register activity, ui/ux improving

--- app/src/main/java/com/example/vaccinationmanagerapp/LoginActivity.kt
+++ app/src/main/java/com/example/vaccinationmanagerapp/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.vaccinationmanagerapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +18,12 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val AccountRegisterClick = findViewById<TextView>(R.id.textViewAccountRegisterClick)
+        AccountRegisterClick.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/vaccinationmanagerapp/RegisterActivity.kt
+++ app/src/main/java/com/example/vaccinationmanagerapp/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.vaccinationmanagerapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +18,12 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val AccountLoginClick = findViewById<TextView>(R.id.textViewAccountLoginClick)
+        AccountLoginClick.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
